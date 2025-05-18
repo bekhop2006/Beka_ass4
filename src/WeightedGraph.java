cf5401f0f3dc70e83f2be798827c3cf5797f1455
beknur <bekhop2006@gmail.com>
2025-05-18 22:24:21 +0500

    zhay update goi solai2

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,2 +1,22 @@
-public class WeightedGraph {
+import java.util.*;
+
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Edge<Vertex<V>>>> map = new HashMap<>();
+
+    public void addVertex(Vertex<V> vertex) {
+        map.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        source.addAdjacentVertex(destination, weight);
+        map.get(source).add(new Edge<>(source, destination, weight));
+    }
+
+    public Map<Vertex<V>, List<Edge<Vertex<V>>>> getGraph() {
+        return map;
+    }
+
+    public Set<Vertex<V>> getVertices() {
+        return map.keySet();
+    }
 }
